04f469e9aeebb60360d3f9cabe794e3d039c1bd2
baniasadi-m <masoud.baniasadi@gmail.com>
2023-07-07 09:42:57 +0330

    update run shell for start and stop compose services

--- run.sh
+++ run.sh
@@ -1,9 +1,31 @@
-#!/bin/bash 
+#!/bin/bash
+########################################
+#######  parsing args first  ###########
+########################################
+
+  while [ $# -gt 0 ];do
+      arg1=$1
+      case $arg1 in 
+        -s)
+	    siganl_compose=$2
+            shift 2
+            ;;
+      esac
+  done
+
+
+###################################################
+######## checking if command is installed #########
+###################################################
+
 
 if which docker > /dev/null 2>&1; then
-    docker compose up -d --quiet-pull
+    if [ $siganl_compose == 'up' ];then
+        docker compose up -d --quiet-pull
+    elif [ $siganl_compose == 'down' ];then
+        docker compose down
+    fi
 else
     echo "docker command does not exist."
     exit 1
 fi
-
