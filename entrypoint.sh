bff2b17f7ed6573ec18df55d40415ad4d9794f86
baniasadi-m <masoud.baniasadi@gmail.com>
2023-12-21 12:16:46 +0330

    update entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ cd vdiManager
 
 python manage.py makemigrations && python manage.py migrate
 
-python manage.py registerlicense -d $DJANGO_LICENSE_DAYS -p $DJANGO_LICENSE_USERS -a $DJANGO_LICENSE_API -N $DJANGO_LICENSE_FULL_NAME -n $DJANGO_LICENSE_SHORT_NAME
+python manage.py registerlicense -d $DJANGO_LICENSE_DAYS -p $DJANGO_LICENSE_USERS -a $DJANGO_LICENSE_API -N "$DJANGO_LICENSE_FULL_NAME" -n "$DJANGO_LICENSE_SHORT_NAME"
 
 if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
     (python manage.py createsuperuser --username=$DJANGO_SUPERUSER_USERNAME --email=$DJANGO_SUPERUSER_EMAIL --noinput)
